f1e184166ad2c18ffc35070e80359f080f29debf
yeri <rlafl7942@naver.com>
2024-03-18 18:06:18 +0900

    solved BOJ_2531

---
+++ baekjoon/투포인터/BOJ_2531.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+sushi=[]
+
+n,d,k,c = map(int, input().split())
+
+for _ in range(n):
+  x = int(input())
+  sushi.append(x)
+
+max_len = 0
+
+for i in range(0,n):
+  check = set()
+  for j in range(4):
+    check.add(sushi[(i+j)%n])
+  if c not in check:
+    check.add(c)
+  max_len=max(max_len, len(check))
+
+print(max_len)
