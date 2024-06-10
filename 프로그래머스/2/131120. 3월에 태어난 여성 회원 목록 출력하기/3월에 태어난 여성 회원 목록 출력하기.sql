04c59186295cd157f03db7babc295841ff0719a8
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-06-10 19:31:59 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+select member_id, member_name, gender, date_format(date_of_birth,"%Y-%m-%d") date_of_birth
+from member_profile
+where date_of_birth like ("%03%") and gender = "W" and tlno is not null
+order by member_id
