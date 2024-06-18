9cafc2fddcaa887e7be9537976bf14339bbb3115
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-06-18 19:51:00 +0900

    [level 2] Title: 짝지어 제거하기, Time: 118.34 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -5,10 +5,8 @@ def solution(s):
         if stack:
             if stack[-1] == s[i]:
                 stack.pop()
-            else:
-                stack.append(s[i])
-        else:
-            stack.append(s[i])
+                continue
+        stack.append(s[i])
     if stack:
         return 0
     else:
