8db81dd64f7c4ddb01b9ae58f32955db1b3d206a
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-08-27 18:10:35 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 272 ms, Memory: 314208 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,13 +1,16 @@
 import sys
-# recursion error 떠서 recursion limit 늘려줌
-sys.setrecursionlimit(10 ** 5)
+sys.setrecursionlimit(10**5)
 
 input = sys.stdin.readline
 
 n = int(input())
-
 graph = [[] for _ in range(n+1)]
 visited = [0] * (n+1)
+
+for i in range(n-1):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
 
 def dfs(v):
   for i in graph[v]:
@@ -15,13 +18,7 @@ def dfs(v):
       visited[i] = v
       dfs(i)
 
-for _ in range(n-1):
-  a, b = map(int, input().split())
-  graph[a].append(b)
-  graph[b].append(a)
-
-
 dfs(1)
 
-for i in range(2, n+1):
+for i in range(2,n+1):
   print(visited[i])
