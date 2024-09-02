c1ab9992e3ffdfe45aa141e95f5676ed15c4112a
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-09-02 23:09:28 +0900

    [Gold V] Title: 토마토, Time: 1936 ms, Memory: 98540 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,35 +2,36 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-
-
-queue = deque()
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 m, n = map(int, input().split())
-tomato = [list(map(int,input().split())) for _ in range(n)]
+graph = []
+for _ in range(n):
+  graph.append(list(map(int, input().split())))
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
+q = deque()
 for i in range(n):
   for j in range(m):
-    if tomato[i][j] == 1:
-      queue.append([i,j]) # 익은 토마토 먼저 queue에 넣어주기
+    if graph[i][j] == 1:
+      q.append((i,j))
 
-while queue:
-  x,y = queue.popleft() # 하나씩 빼서 확인
-  for i in range(4): # 상하좌우 확인
-    nx, ny = dx[i]+x, dy[i]+y
-    if nx>=0 and nx<n and ny>=0 and ny<m:
-      if tomato[nx][ny] == 0:
-        tomato[nx][ny] = tomato[x][y]+1 # 날짜수 더해주기
-        queue.append([nx,ny]) # 익었으니까 queue에 넣어주기
+while q:
+  x, y = q.popleft()
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
 
-ans = 0
-for i in tomato:
-  for j in i:
-    if j == 0: # 안익은거 있으면 -1
+    if 0<=nx<n and 0<=ny<m:
+      if graph[nx][ny] == 0:
+        graph[nx][ny] = graph[x][y] + 1
+        q.append((nx, ny))
+
+answer = 0
+for i in range(n):
+  for j in range(m):
+    if graph[i][j] == 0:
       print(-1)
       exit()
-
-  ans = max(ans, max(i)) 
-
-print(ans - 1)
+  answer = max(answer, max(graph[i]))
+print(answer-1)
