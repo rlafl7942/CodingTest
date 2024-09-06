5c99aa143c656d7c0a509f4f0a99e4515dd301ce
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-09-06 23:25:52 +0900

    [Silver II] Title: 유기농 배추, Time: 56 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -3,41 +3,41 @@ from collections import deque
 
 input = sys.stdin.readline
 
-t = int(input())
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-def bfs(x, y):
+def BFS(i, j):
   q = deque()
-  q.append((x, y))
+  q.append((i,j))
   cnt = 1
 
   while q:
-    a, b = q.popleft()
+    x, y = q.popleft()
     for i in range(4):
-      nx = a + dx[i]
-      ny = b + dy[i]
+      nx = x + dx[i]
+      ny = y + dy[i]
 
-      if nx<0 or nx>=n or ny<0 or ny>=m:
-        continue
-      if graph[nx][ny] == 1:
-        cnt+=1
-        graph[nx][ny] = 0
-        q.append((nx, ny))
+      if nx>=0 and nx<n and ny>=0 and ny<m:
+        if farm[nx][ny] == 1:
+          farm[nx][ny] += 1
+          q.append((nx, ny))
+          cnt+=1
 
   return cnt
 
+
+t = int(input())
 for _ in range(t):
   n, m, k = map(int, input().split())
-  graph = [[0 for _ in range(m)] for _ in range(n)]
-  answer = []
+  farm = [[0 for _ in range(m)] for _ in range(n)]
   for _ in range(k):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
+    x, y = map(int, input().split())
+    farm[x][y] = 1
 
+  answer = []
   for i in range(n):
     for j in range(m):
-      if graph[i][j] == 1:
-        answer.append(bfs(i,j))
+      if farm[i][j] == 1:
+        answer.append(BFS(i, j))
+  
   print(len(answer))
