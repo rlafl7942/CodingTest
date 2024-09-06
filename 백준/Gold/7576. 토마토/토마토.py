1e70f22b897cc0bf769cfa52bbff4ae5ea84a04c
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-09-06 23:43:28 +0900

    [Gold V] Title: 토마토, Time: 1364 ms, Memory: 98600 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,36 +2,43 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-m, n = map(int, input().split())
-graph = []
-for _ in range(n):
-  graph.append(list(map(int, input().split())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+n, m = map(int, input().split())
+graph = []
 q = deque()
-for i in range(n):
-  for j in range(m):
+for _ in range(m):
+  tomato = list(map(int, input().split()))
+  graph.append(tomato)
+
+for i in range(m):
+  for j in range(n):
     if graph[i][j] == 1:
-      q.append((i,j))
+      q.append((i, j))
 
-while q:
-  x, y = q.popleft()
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
+def bfs():
+  while q:
+    x, y = q.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
 
-    if 0<=nx<n and 0<=ny<m:
-      if graph[nx][ny] == 0:
-        graph[nx][ny] = graph[x][y] + 1
-        q.append((nx, ny))
+      if nx>=0 and nx<m and ny>=0 and ny<n:
+        if graph[nx][ny] == 0:
+          graph[nx][ny] = graph[x][y] + 1
+          q.append((nx, ny))
+
+bfs()
 
 answer = 0
-for i in range(n):
-  for j in range(m):
+for i in range(m):
+  for j in range(n):
     if graph[i][j] == 0:
       print(-1)
       exit()
-  answer = max(answer, max(graph[i]))
+    else:
+      answer = max(answer, graph[i][j])
+
 print(answer-1)
