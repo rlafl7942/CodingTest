310d817fc6ece3aa1b4ba358a404c1790fff937b
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-09-11 21:17:50 +0900

    [Gold V] Title: 토마토, Time: 3336 ms, Memory: 48588 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -3,35 +3,46 @@ from collections import deque
 
 input = sys.stdin.readline
 
+m, n, h = map(int, input().split())
+graph = []
+q = deque()
+for _ in range(h):
+  tmp = []
+  for _ in range(n):
+    tomato = list(map(int, input().split()))
+    tmp.append(tomato)
+  graph.append(tmp)
 
-queue = deque()
-dx, dy, dz = [-1, 1, 0, 0, 0, 0], [0, 0, -1, 1, 0, 0], [0, 0, 0, 0, -1, 1]
-m, n, o = map(int, input().split())
-tomato = [[list(map(int, input().split())) for _ in range(n)] for _ in range(o)]
+dx = [1, -1, 0, 0, 0, 0]
+dy = [0, 0, 1, -1, 0, 0]
+dz = [0, 0, 0, 0, 1, -1]
 
-for i in range(o):
+for i in range(h):
   for j in range(n):
     for k in range(m):
-      if tomato[i][j][k] == 1:
-        queue.append([i,j,k])
+      if graph[i][j][k] == 1:
+        q.append((i, j, k))
 
+while q:
+  z, y, x = q.popleft()
+  for i in range(6):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    nz = z + dz[i]
 
-while queue:
-  x,y,z = queue.popleft() # 하나씩 빼서 확인
-  for i in range(6): 
-    nx, ny, nz = dx[i]+x, dy[i]+y, dz[i]+z
-    if nx>=0 and nx<o and ny>=0 and ny<n and nz>=0 and nz<m:
-      if tomato[nx][ny][nz] == 0:
-        tomato[nx][ny][nz] = tomato[x][y][z]+1 # 날짜수 더해주기
-        queue.append([nx,ny,nz]) # 익었으니까 queue에 넣어주기
+    if nx>=0 and nx<m and ny>=0 and ny<n and nz>=0 and nz<h:
+      if graph[nz][ny][nx] == 0:
+        graph[nz][ny][nx] = graph[z][y][x] + 1
+        q.append((nz, ny, nx))
 
-ans = 0
+tmp = 0
+for i in range(h):
+  for j in range(n):
+    for k in range(m):
+      if graph[i][j][k] == 0:
+        print(-1)
+        exit()
+      if tmp < graph[i][j][k]:
+        tmp = graph[i][j][k]
 
-for i in tomato:
-  for j in i:
-    if 0 in j:
-      print(-1)
-      exit()
-    ans = max(ans, max(j)) 
-
-print(ans - 1)
+print(tmp-1)
