6aa1a3ffacc39e6754b70e0440bfe0d4d8854526
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-10-04 19:02:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.43 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,69 +1,31 @@
-# from collections import deque
-
-# def solution(maps):
-#     dx=[-1, 1, 0, 0]
-#     dy=[0, 0, -1, 1]
-    
-#     queue = deque()
-#     queue.append((0,0))
-    
-#     check = [[0]*5 for _ in range(5)]
-#     tmp = [[0]*5 for _ in range(5)]
-    
-#     tmp[0][0] = 1
-#     check[0][0] = 1
-    
-#     while queue:
-#         now = queue.popleft()
-        
-#         for i in range(4):
-#             x = now[0] + dx[i]
-#             y = now[1] + dy[i]
-            
-#             if 0<=x<5 and 0<=y<5:
-#                 if check[x][y] == 0 and maps[x][y] == 1:
-#                     check[x][y] = 1 # 들렀다고 표시
-#                     tmp[x][y] = tmp[now[0]][now[1]]+1
-#                     queue.append((x,y))
-    
-#     if tmp[4][4] == 0:
-#         return -1
-#     else:
-#         return tmp[4][4]
-
 from collections import deque
-
 def solution(maps):
-
-    dx = [-1,0,1,0]
-    dy = [0,1,0,-1]
-
-    result = 0
-
     n = len(maps)
     m = len(maps[0])
-
-    ck = [[0]*m for _ in range(n)]
-    dis = [[0]*m for _ in range(n)]
-    queue = deque()
-
-    ck[0][0] = 1
-    dis[0][0] = 1
-    queue.append((0,0))
-
-    while queue:
-        now = queue.popleft()
+    graph = []
+    q = deque()
+    
+    for i in range(n):
+        graph.append(maps[i])
+        
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    
+    q.append((0, 0))
+    
+    while q:
+        x, y = q.popleft()
 
         for i in range(4):
-            x = now[0]+dx[i]
-            y = now[1]+dy[i]
-
-            if 0<=x<=n-1 and 0<=y<=m-1:
-                if ck[x][y] == 0 and maps[x][y] == 1:
-                    ck[x][y] = 1
-                    dis[x][y] = dis[now[0]][now[1]] + 1
-                    queue.append((x,y))
-
-    result = -1 if dis[n-1][m-1] == 0 else dis[n-1][m-1]
-
-    return result
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx>=0 and nx<n and ny>=0 and ny<m:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = graph[x][y] + 1
+                    q.append((nx, ny))
+            
+    if graph[n-1][m-1] == 1:
+        return -1
+    else:
+        return graph[n-1][m-1]
