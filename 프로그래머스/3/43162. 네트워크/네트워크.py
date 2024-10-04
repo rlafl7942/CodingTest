90d1d3d0acc084e0f0bfee78294f304d82d77c0b
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-10-05 01:17:44 +0900

    [level 3] Title: 네트워크, Time: 1.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,39 +1,32 @@
 from collections import deque
-
 def solution(n, computers):
     graph = [[] for _ in range(n)]
-    visited = [False] * n
+    visited = [False] * (n)
     q = deque()
     
     for i in range(len(computers)):
         for j in range(len(computers[i])):
             if i!=j:
-                if computers[i][j]==1:
+                if computers[i][j] == 1:
                     graph[i].append(j)
-    
+                
     def bfs(v):
         q.append(v)
         cnt=0
-        if visited[v] == False:
+        if not visited[v]:
             visited[v] = True
-           
-            while q:
-                tmp = q.popleft()
-                for i in graph[tmp]:
-                    if not visited[i]:
-                        q.append(i)
-                        cnt+=1
-                        visited[i] = True
-        return cnt
-
-    print(graph)
-    l=[]
-    for i in range(0, n):
-        tmp = bfs(i)
-        l.append(tmp)
-    
-    return n-sum(l)
             
-        
-        
+        while q:
+            tmp = q.popleft()
+            for i in graph[tmp]:
+                if not visited[i]:
+                    visited[i] = True
+                    q.append(i)
+                    cnt+=1
+        return cnt
     
+    answer = []
+    for i in range(0, n):
+        answer.append(bfs(i))
+        
+    return n-sum(answer)
