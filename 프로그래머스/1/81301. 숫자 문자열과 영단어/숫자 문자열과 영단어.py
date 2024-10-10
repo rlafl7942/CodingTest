95bb31f52600535b6f7890b23163980bef0685d1
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-10-10 20:26:15 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,6 @@
+def solution(s):
+    dic = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    
+    for value in dic:
+        s = s.replace(value, dic[value])
+    return int(s)
