708639c8d866f0454baebddf1b134c3bb12205df
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-10-18 16:51:53 +0900

    [level 2] Title: 전화번호 목록, Time: 99.56 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,7 @@
 def solution(phone_book):
     answer = True
-    phone_book.sort()
+    phone_book = sorted(phone_book)
     for i in range(len(phone_book)-1):
         if phone_book[i+1].startswith(phone_book[i]):
-            answer = False
-            break
+            return False
     return answer
-        
