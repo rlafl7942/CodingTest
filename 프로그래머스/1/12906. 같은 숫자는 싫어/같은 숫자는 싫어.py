afe1b45dbaebc62e68c431921dcaffe67cb72e3e
Yeri Kim <46741373+rlafl7942@users.noreply.github.com>
2024-10-18 19:01:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 65.62 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,7 +1,9 @@
 def solution(arr):
-    answer=[]
-    answer.append(arr[0])
-    for i in range(1, len(arr)):
-        if arr[i-1] != arr[i]:
-            answer.append(arr[i])
+    answer = []
+    for i in arr:
+        if answer:
+            if answer[-1] != i:
+                answer.append(i)
+        else:
+            answer.append(i)
     return answer
